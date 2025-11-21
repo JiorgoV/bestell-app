94266e16fa60b458efee3e1a88eb6f341e91bc61
Georgios Vavitsas <g.vavitsas@gmail.com>
2025-11-21 19:29:40 +0100

    templates-loaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 
+function init() {
+    renderDishes();
+    renderDrinks();
+    renderToppings();
+    renderDesserts();
+}
+
 function renderDishes() {
     let dishesRef = document.getElementById('dishesContent')
     dishesRef.innerHTML = "";
@@ -8,3 +15,30 @@ function renderDishes() {
     }
 }
 
+function renderDrinks() {
+    let drinksRef = document.getElementById('drinksContent')
+    drinksRef.innerHTML = "";
+
+    for (let indexDrinks = 0; indexDrinks < myDishes[0].menu.drinks.length; indexDrinks++) {
+        drinksRef.innerHTML += getDrinkTemplate(indexDrinks);
+        
+    }
+}
+
+function renderToppings() {
+    let toppingsRef = document.getElementById('toppingsContent')
+    toppingsRef.innerHTML = "";
+
+    for (let indexToppings = 0; indexToppings < myDishes[0].menu.toppings.length; indexToppings++) {
+        toppingsRef.innerHTML += getToppingTemplate(indexToppings);   
+    }
+}
+
+function renderDesserts() {
+    let dessertsRef = document.getElementById('dessertsContent')
+    dessertsRef.innerHTML = "";
+
+    for (let indexDesserts = 0; indexDesserts < myDishes[0].menu.toppings.length; indexDesserts++) {
+        dessertsRef.innerHTML += getDessertTemplate(indexDesserts);
+    }
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,54 @@
 
 function getDishTemplate(indexDishes){
-    return `
+    return `<div class="dish-card">
+        <div class="dish-description">
+            <h3>${myDishes[0].menu.dishes[indexDishes].name}</h3>
+            <p>${myDishes[0].menu.dishes[indexDishes].description}</p>
+        </div>
+        <div class="button-price">
+            <button class="add-button">+</button>
+            <p class="price">${myDishes[0].menu.dishes[indexDishes].price.toFixed(2)}€</p>
+        </div>
+    </div>
+    `
+}
+
+function getDrinkTemplate(indexDrinks) {
+    return `<div class="dish-card">
+        <div class="dish-description">
+            <h3>${myDishes[0].menu.drinks[indexDrinks].name}</h3>
+        </div>
+        <div class="button-price">
+            <button class="add-button">+</button>
+            <p class="price">${myDishes[0].menu.drinks[indexDrinks].price.toFixed(2)}€</p>
+        </div>
+    </div>
+    `
+}
+
+function getToppingTemplate(indexToppings) {
+    return `<div class="dish-card">
+        <div class="dish-description">
+            <h3>${myDishes[0].menu.toppings[indexToppings].name}</h3>
+        </div>
+        <div class="button-price">
+            <button class="add-button">+</button>
+            <p class="price">${myDishes[0].menu.toppings[indexToppings].price.toFixed(2)}€</p>
+        </div>
+    </div>
+    `
+}
+
+function getDessertTemplate(indexDesserts) {
+    return `<div class="dish-card">
+        <div class="dish-description">
+            <h3>${myDishes[0].menu.desserts[indexDesserts].name}</h3>
+            <p>${myDishes[0].menu.desserts[indexDesserts].description}</p>
+        </div>
+        <div class="button-price">
+            <button class="add-button">+</button>
+            <p class="price">${myDishes[0].menu.desserts[indexDesserts].price.toFixed(2)}€</p>
+        </div>
+    </div>
     `
 }
